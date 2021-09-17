7c4f6bdf0866ce088e68ba9150ce9ef148c10083
Gonzalo <Bernad>
2021-09-17 18:19:13 -0300

    Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'codeChallenge';
+  gradeA;
+  gradeB;
+  gradeC;
+  
+  constructor(){
+    this.gradeA = [12, 15, 11, 17, 19];
+    this.gradeB = [10, 15, 17, 13, 19];
+    this.gradeC = [18, 15, 19, 17, 14];
+    this.uniqueGradeValues(this.gradeA, this.gradeB, this.gradeC);
+  }
+
+  private uniqueGradeValues(array1: any, array2: any, array3: any ) {
+    array1.concat(array2)
+    let arrayBuffer = [];
+    arrayBuffer = [...array1, ...array2, ...array3];
+
+    let output2 = [];
+    arrayBuffer.filter( item => {
+      if (arrayBuffer.filter(a => a == item).length < 2) 
+        output2 = [...output2, item];
+    });
+    // console.table(arrayBuffer);
+    console.log('unique gradevalues', output2);
+    return output2;
+  }
 }
